d1dbc1382f0030414403f94fd03bf7e61b65d1a8
Apostol Marinov <a.marinov@credoweb.bg>
2019-04-25 09:18:59 +0300

    added docker container entrypoint with options for initial project setup and db seeding; added adminer tool to docker-compose example file

---
+++ docker/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [[ -n "${BUILD}" ]]; then
+  cd /var/www/html
+  composer install
+fi
+
+if [[ -n "${INITIAL}" ]]; then
+    cd /var/www/html
+
+    if [[ -f .env ]]; then
+        export $(cat .env | xargs)
+
+        if [[ -z "${APP_KEY}" ]]; then
+            composer install
+            php artisan key:generate
+
+            if [[ -n "${SEED}" ]]; then
+                php artisan migrate:fresh --seeder=AppSeeder
+            else
+                php artisan migrate:fresh
+            fi
+        fi
+    fi
+fi
+
+apache2ctl -D FOREGROUND
