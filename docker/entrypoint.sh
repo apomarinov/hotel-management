c0875f7f65d7a6fe522415a924fd42f8afaa25f9
Apostol Marinov <a.marinov@credoweb.bg>
2019-04-27 16:30:29 +0300

    added debug messages to dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
+# colors
+BLUE='\033[1;36m'
+GREEN='\033[1;32m'
+NC='\033[0m'
+
 # used when doploying
 if [[ -n "${BUILD}" ]]; then
+  printf "${GREEN} BUILD ${NC}\n"
   cd /var/www/html
+
+  printf "${BLUE} COMPOSER ${NC}\n"
   composer install
+
+  printf "${BLUE} NPM INSTALL ${NC}\n"
   npm install
+
+  printf "${BLUE} NPM BUILD ${NC}\n"
   npm run prod
 fi
 
@@ -16,22 +28,36 @@ if [[ -n "${INITIAL}" ]]; then
         export $(cat .env | xargs)
 
         if [[ -z "${APP_KEY}" ]]; then
+            printf "${GREEN} INITIAL ${NC}\n"
+
+            printf "${BLUE} COMPOSER ${NC}\n"
             composer install
+
+            printf "${BLUE} NPM INSTALL ${NC}\n"
             npm install
+
+            printf "${BLUE} NPM BUILD ${NC}\n"
             npm run prod
+
+            printf "${BLUE} KEYGEN ${NC}\n"
             php artisan key:generate
-            php artisan config:cache
 
             if [[ -n "${SEED}" ]]; then
+                printf "${BLUE} MIGRATE AND SEED ${NC}\n"
                 php artisan migrate:fresh --seeder=AppSeeder
             else
+                printf "${BLUE} MIGRATE ${NC}\n"
                 php artisan migrate:fresh
             fi
+
+            printf "${BLUE} CACHE ${NC}\n"
+            php artisan config:cache
         fi
     fi
 fi
 
 # if we are not deploying, start apache
 if [[ -z "${BUILD}" ]]; then
-  apache2ctl -D FOREGROUND
+    printf "${BLUE} APACHE ${NC}\n"
+    apache2ctl -D FOREGROUND
 fi
