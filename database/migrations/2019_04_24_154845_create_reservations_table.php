c296d98714d2df9f110be2054fe9be031db8ffd4
Apostol Marinov <a.marinov@credoweb.bg>
2019-04-29 15:02:28 +0300

    fix table name in down method

--- database/migrations/2019_04_24_154845_create_reservations_table.php
+++ database/migrations/2019_04_24_154845_create_reservations_table.php
@@ -32,6 +32,6 @@ class CreateReservationsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('bookings');
+        Schema::dropIfExists('reservations');
     }
 }
