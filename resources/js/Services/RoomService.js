4f246772ddf7a6ff83c3300eb638df56e7ef0f4a
Apostol Marinov <a.marinov@credoweb.bg>
2019-04-29 14:24:16 +0300

    fix availoable room listing

--- resources/js/Services/RoomService.js
+++ resources/js/Services/RoomService.js
@@ -28,4 +28,23 @@ export default class RoomService extends ResourceService {
     static getAvailableRooms(data) {
         return this.list(this.availableRoomsUrl(), data);
     }
+
+    /**
+     * Get room listing
+     *
+     * @param url
+     * @param data
+     * @returns {Promise<any>}
+     */
+    static list(url, data) {
+        return new Promise((resolve, reject) => {
+            axios.get(url, {params: data})
+                .then(response => {
+                    resolve(response.data);
+                })
+                .catch(error => {
+                    reject(error.response.data);
+                });
+        });
+    }
 }
